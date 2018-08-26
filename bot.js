77f2ef1142a9bd66267eba71afb84273a22d156e
shaaaaaaq <42686442+shaaaaaaq@users.noreply.github.com>
2018-08-26 07:49:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -341,5 +341,32 @@ client.on('message', message => {
       message.channel.send(helpEmbed);
     }
 });
+client.on('message', async message => {
+            if(!message.channel.guild) return;
+             if (message.content.startsWith("Hsets")) {
+let args = message.content.split(' ').slice(1).join(' ');
+            let sigMessage = await args;
+            
+            if (sigMessage === "online") {
+                client.user.setStatus("online");
+                message.author.send("Your status was set to online.");
+            }
+            if (sigMessage === "idle") {
+                client.user.setStatus("idle");
+                message.author.send("Your status was set to idle.");
+            }
+            if (sigMessage === "invisible") {
+                client.user.setStatus("invisible");
+                message.author.send("Your status was set to invisible.");
+            }
+            if (sigMessage === "dnd") {
+                client.user.setStatus("dnd");
+                message.author.send("Your status was set to dnd.");
+            }
+            // message.author.send("." + message.content);
+        
+}
+});
+
 
 client.login(process.env.BOT_TOKEN);
